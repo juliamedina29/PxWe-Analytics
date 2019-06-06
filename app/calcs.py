aea106360feeccc2a2c6096709043d55587deccc
juliamedina29 <julia.medina@wework.com>
2019-06-06 13:09:32 -0400

    Working site

--- app/calcs.py
+++ app/calcs.py
@@ -14,14 +14,14 @@ import pandas as pd
 #proj name for indexing#
 proj_name_hc = "51 Melcher Street"
 num = main.proj_list.index(proj_name_hc)
-num = 0
+num = 50
 name = main.proj_list[num]
 cur_proj_df = main.proj_dfs[num]
 cur_proj_desk_count = cur_proj_df['desk_count'].sum()
 #set up list of space types in the project#
-cur_proj_space_list = main.space_type_dict[proj_name_hc]
+cur_proj_space_list = main.space_type_dict[name]
 #set up list of area sums for each type#
-cur_proj_space_area_sums_list = main.proj_areas_dict[proj_name_hc]
+cur_proj_space_area_sums_list = main.proj_areas_dict[name]
 #create new df which only includes area sums per space type#
 cur_proj_sum_df = pd.DataFrame(cur_proj_space_area_sums_list, index=cur_proj_space_list)
 #count the floors#
@@ -34,7 +34,6 @@ cur_proj_color_list = []
 for x in cur_proj_space_list : 
     cur_proj_color_list.append(main.space_colors_dict[x])
 
-output_file("app/templates/graph.html")
 
 keys = cur_proj_space_list
 values = cur_proj_space_area_sums_list
@@ -51,6 +50,7 @@ p.wedge(x=0, y=1, radius=0.4,
         start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
         line_color="white", fill_color='color', legend='space', source=data)
 
+output_file("app/templates/graph.html")
 p.axis.axis_label=None
 p.axis.visible=False
 p.grid.grid_line_color = None
